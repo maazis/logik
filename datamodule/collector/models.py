e219a82b7ff9485bf8e7b3c606f3e628bbb260c5
maazismail <58908625+maazismail@users.noreply.github.com>
2021-07-30 13:56:35 +0500

    fix intalled app error

--- datamodule/collector/models.py
+++ datamodule/collector/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Viewer(models.Model):
+    survey_age = models.FloatField()
+    survey_gender = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.id
