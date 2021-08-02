db043b97f6a824a62e726ef2fff87b05e8da8844
maazismail <58908625+maazismail@users.noreply.github.com>
2021-08-02 16:10:12 +0500

    monday backlog

--- datamodule/collector/views.py
+++ datamodule/collector/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views import View
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class Index(View):
+    template = 'index.html'
+
+    def get(self, request):
+        return render(request, self.template)   
