eb752779481decfb2cf714030c717cbeb7e15d80
maazismail <58908625+maazismail@users.noreply.github.com>
2021-08-02 17:18:05 +0500

    integrate OCV python app

--- datamodule/collector/views.py
+++ datamodule/collector/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.views import View
+import cv2 as cv
+import random
 
+def index(request):
+    return render(request,"index.htm")
 
-class Index(View):
-    template = 'index.html'
+def input(request):
+    if request.method=="POST":
+        return render(request,"input.htm",{
+            "gender":request.POST.get("gender"),
+            "age":request.POST.get("age")
+        })
+    return render(request,"input.htm",{
+        "gender":"",
+        "age":""
+    })
 
-    def get(self, request):
-        return render(request, self.template)   
+def camera(request):
+    runCamera()
+    return render(request,"camera.htm")
+
+def runCamera():
+    cap = cv.VideoCapture(0)
+
+    fourcc = cv.VideoWriter_fourcc(*'MJPG') 
+    out = cv.VideoWriter('output'+str(random.randint(0,30))+'.avi', fourcc, 20.0, (640, 480))
+
+    while cap.isOpened():
+        ret , frame = cap.read()
+        if ret==True:
+            cv.imshow("frame",frame)
+        
+            out.write(frame)
+
+            if cv.waitKey(25) & 0xFF == ord('q'):
+                break
+                
+        else:
+            break
+
+    cap.release()
+    out.release()
+    cv.destroyAllWindows()
