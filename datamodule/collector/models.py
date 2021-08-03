439281f843c7ce3ee41fb894c393090c46d51983
maazismail <58908625+maazismail@users.noreply.github.com>
2021-08-03 13:17:35 +0500

    fix null entry

--- datamodule/collector/models.py
+++ datamodule/collector/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Viewer(models.Model):
-    survey_age = models.FloatField()
-    survey_gender = models.CharField(max_length=15)
+    survey_age = models.FloatField(null=True, blank=True)
+    survey_gender = models.CharField(max_length=15, null=True, blank=True)
